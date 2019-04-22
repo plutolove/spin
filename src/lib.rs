b16da86c2dd8f31b8ede15312deb6e42c260b6f0
plutolove <sa517255@mail.ustc.edu.cn>
2019-04-22 14:24:55 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,10 @@
-pub mod mutex;
-pub mod rwlock;
+#![crate_type = "lib"]
+#![no_std]
+
 
 #[cfg(test)]
-mod tests {
-    struct Node {
-        x: u32,
-    }
-    impl Node {
-        fn inc(&mut self) {
-            self.x += 1;
-        }
-        fn get_data(&self) -> u32 {
-            self.x
-        }
-    }
+#[macro_use]
+extern crate std;
 
-    #[test]
-    fn test_mutex1() {
-        use crate::mutex::Mutex;
-        use std::thread;
-
-        static M: Mutex<Node> = Mutex::new(Node{x: 0});
-        const J: u32 = 10;
-        const K: u32 = 1;
-        fn inc() {
-            for _ in 0..J {
-                {
-                    let mut tmp = M.lock();
-                    tmp.inc();
-                    println!("x: {}", tmp.get_data());
-                }
-            }
-        }
-
-        for _ in 0..K {
-            thread::spawn(move|| { inc();}).join().unwrap();
-            thread::spawn(move|| { inc();}).join().unwrap();
-        }
-    }
-
-    #[test]
-    fn test_mutex2() {
-        use crate::mutex::{Mutex};
-        use std::thread;
-        use core::ops::DerefMut;
-
-        static MUTEX: Mutex<u32> = Mutex::new(0);
-        const N: u32 = 10;
-        const T: u32 = 5;
-
-        fn inc(i: u32) {
-            for _ in 0..N {
-                {
-                    let mut xx = MUTEX.lock();
-                    (*xx.deref_mut()) += i;
-                    println!("tmp: {}", xx.deref_mut());
-                }
-            }
-        }
-
-        for _ in 0..T {
-            thread::spawn(move|| { inc(1);}).join().unwrap();
-            thread::spawn(move|| { inc(2);}).join().unwrap();
-        }
-
-    }
-}
+pub mod mutex;
+pub mod rwlock;
--- src/mutex.rs
+++ src/mutex.rs
@@ -91,3 +91,67 @@ impl <'a, T: ?Sized> Drop for Lock<'a, T> {
         self.lock.store(false, Ordering::Release);
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::mutex::{Mutex};
+    use std::thread;
+    use core::ops::DerefMut;
+    
+    struct Node {
+        x: u32,
+    }
+    impl Node {
+        fn inc(&mut self) {
+            self.x += 1;
+        }
+        fn get_data(&self) -> u32 {
+            self.x
+        }
+    }
+
+    #[test]
+    fn test_mutex1() {
+        static M: Mutex<Node> = Mutex::new(Node{x: 0});
+        const J: u32 = 10;
+        const K: u32 = 1;
+        fn inc() {
+            for _ in 0..J {
+                {
+                    let mut tmp = M.lock();
+                    tmp.inc();
+                }
+            }
+        }
+
+        for _ in 0..K {
+            thread::spawn(move|| { inc();}).join().unwrap();
+            thread::spawn(move|| { inc();}).join().unwrap();
+        }
+        assert_eq!(M.lock().get_data(), 20);
+    }
+
+    #[test]
+    fn test_mutex2() {
+
+        static MUTEX: Mutex<u32> = Mutex::new(0);
+        const N: u32 = 10;
+        const T: u32 = 5;
+
+        fn inc(i: u32) {
+            for _ in 0..N {
+                {
+                    let mut xx = MUTEX.lock();
+                    (*xx.deref_mut()) += i;
+                }
+            }
+        }
+
+        for _ in 0..T {
+            thread::spawn(move|| { inc(1);}).join().unwrap();
+            thread::spawn(move|| { inc(2);}).join().unwrap();
+        }
+        assert_eq!(*MUTEX.lock().deref_mut(), 150);
+    }
+}
